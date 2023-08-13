2f75780294e2dbc2b1e96077ae13403c97b1c645
gastephano <gastephano@gmail.com>
2023-08-12 21:17:38 -0300

    Adds labels and returns to translator

--- translator.c
+++ translator.c
@@ -2,8 +2,8 @@
 #include <string.h>
 #define LINEMAX 100
 int main() {
-    char fonte[100] = "fonte.fth";
-    char resultado[100] = "resul.s";
+    char fonte[LINEMAX] = "fonte.fth";
+    char resultado[LINEMAX] = "resul.s";
     FILE* entrada;
     FILE* saida;
     entrada = fopen(fonte, "r");
@@ -13,25 +13,19 @@ int main() {
         while (symbol != EOF){
             fgets(symbol, LINEMAX, entrada);
             if (isNumber(symbol) == 1)
-                fprintf(saida, "mov r0, #%s\n push{r0}\n", symbol);
-            //else if (symbol == ":")
-            //{
-            //    escreve na tabela de simbolos essa label
-            //}
-            //
-            //else if (symbol == tabela de simbolos)
-            //{
-            //    escreve no arquivo {bl symbol}
-            //}
+                fprintf(saida, "mov r0, #%s\n push{r0}\n", symbol); // push number in stack
+            else if (symbol == ";")
+                fprintf(saida, "mov pc, lr\n"); // function return
             else
-                printf("simbolo inválido\n");
-                return -1;
+                fprintf(saida, "bl %s\n", symbol); // branch to a label
         };
     }
     else {
         printf("arquivo não encontrado\n");
         return -1;
     }
+    fclose(entrada);
+    fclose(saida);
     return 0;
 }
 char isNumber(char *text)
