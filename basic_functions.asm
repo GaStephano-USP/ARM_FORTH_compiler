292db20517e7f18e0a61638033238bc97799eb4a
gastephano <gastephano@gmail.com>
2023-08-13 15:37:12 -0300

    redo last commit, erases not used functions

--- basic_functions.asm
+++ basic_functions.asm
@@ -17,37 +17,69 @@ dup:
     pop {r0}
     push {r0}
     push {r0}
+    mov pc, lr
 
 neg:
     pop {r0}
     mov r1, #0
     sub, r0, r1, r0
     push {r0}
+    mov pc, lr
 
 mul:
     pop {r0, r1}
     mul r0, r0, r1
     push {r0}
+    mov pc, lr
 
 div:
     pop {r0, r1}
     div r0, r1, r0
     push {r0}
+    mov pc, lr
 
 rot:
     pop {r0, r1, r2}
     push {r1}
     push {r0}
     push {r2}
+    mov pc, lr
 
 swap:
     pop {r0, r1}
     push {r0}
     push {r1}
+    mov pc, lr
 
 drop:
     add sp, #4
     @ /* Movo stack pointer pra baixo */
+    mov pc, lr
+
+over:
+    pop {r0, r1}
+    push {r1}
+    push {r0}
+    push {r1}
+    mov pc, lr
+
+pick:
+    pop {r0}
+    mov r1, #4
+    mul r0, r0, r1
+    add sp, sp, r0
+    ldr r1, [sp]
+    sub sp, sp, r0
+    push {r1}
+    mov pc, lr
+
+tuck:
+    pop {r0}
+    pop {r1}
+    push {r0}
+    push {r1}
+    push {r0}
+    mov pc, lr
 
 @ /* Operacoes logicas */
 
@@ -62,18 +94,7 @@ drop:
     mov r2, #1
     pop {r2}
     end
-
->=:
-    pop {r0, r1}
-    cmp r1, r0
-    bge greater_equal
-    mov r2, #0
-    pop {r2}
-    b end
-    greater_equal:
-    mov r2, #1
-    pop {r2}
-    end
+    mov pc, lr
 
 <:
     pop {r0, r1}
@@ -86,20 +107,9 @@ drop:
     mov r2, #1
     pop {r2}
     end
+    mov pc, lr
 
-<=:
-    pop {r0, r1}
-    cmp r1, r0
-    ble less_equal
-    mov r2, #0
-    pop {r2}
-    b end
-    less_equal:
-    mov r2, #1
-    pop {r2}
-    end
-
-==:
+=:
     pop {r0, r1}
     cmp r1, r0
     beq equal
@@ -110,8 +120,9 @@ drop:
     mov r2, #1
     pop {r2}
     end
+    mov pc, lr
 
-!=:
+<>:
     pop {r0, r1}
     cmp r1, r0
     bne nequal
@@ -122,3 +133,4 @@ drop:
     mov r2, #1
     pop {r2}
     end
+    mov pc, lr
