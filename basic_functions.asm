30e288fcc2beb8b8018dba024acd683bc258f669
gastephano <gastephano@gmail.com>
2023-08-13 13:07:22 -0300

    Implements tuck, return basic functions

--- basic_functions.asm
+++ basic_functions.asm
@@ -17,43 +17,51 @@ dup:
     pop {r0}
     push {r0}
     push {r0}
+    mov pc, lr
 
 neg:
     pop {r0}
     mov r1, #0
     sub, r0, r1, r0
     push {r0}
+    mov pc, lr
 
 mul:
     pop {r0, r1}
     mul r0, r0, r1
     push {r0}
+    mov pc, lr
 
 div:
     pop {r0, r1}
     sdiv r0, r1, r0
     push {r0}
+    mov pc, lr
 
 rot:
     pop {r0, r1, r2}
     push {r1}
     push {r0}
     push {r2}
+    mov pc, lr
 
 swap:
     pop {r0, r1}
     push {r0}
     push {r1}
+    mov pc, lr
 
 drop:
     add sp, #4
     @ /* Movo stack pointer pra baixo */
+    mov pc, lr
 
 over:
     pop {r0, r1}
     push {r1}
     push {r0}
     push {r1}
+    mov pc, lr
 
 pick:
     pop {r0}
@@ -63,3 +71,12 @@ pick:
     ldr r1, [sp]
     sub sp, sp, r0
     push {r1}
+    mov pc, lr
+
+tuck:
+    pop {r0}
+    pop {r1}
+    push {r0}
+    push {r1}
+    push {r0}
+    mov pc, lr
