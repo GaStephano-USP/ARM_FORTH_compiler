50e62b306dba6de95a7b58fb7aa5fb389b7cd196
gastephano <gastephano@gmail.com>
2023-08-13 10:47:03 -0300

    Implements filepath to args, and starts IF

--- translator.c
+++ translator.c
@@ -1,21 +1,33 @@
 #include <stdio.h>
 #include <string.h>
 #define LINEMAX 100
-int main() {
-    char fonte[LINEMAX] = "fonte.fth";
-    char resultado[LINEMAX] = "resul.s";
+int main(int args, char *argv[]) {
+    char fonte[LINEMAX] = argv[0];
+    char resultado[LINEMAX] = argv[1];
     FILE* entrada;
     FILE* saida;
     entrada = fopen(fonte, "r");
     saida = fopen(resultado, "w");
     if (entrada != NULL) {
         char symbol[LINEMAX];
+        int num = 0;
         while (symbol != EOF){
             fgets(symbol, LINEMAX, entrada);
             if (isNumber(symbol) == 1)
-                fprintf(saida, "mov r0, #%s\n push{r0}\n", symbol); // push number in stack
+                fprintf(saida, "\tmov r0, #%s\n\t push{r0}\n", symbol); // push number in stack
+            else if (symbol == "IF");
+            //    fprintf(saida, "\tbz L%d\n", num);
+            else if (symbol == "ELSE");
+            //    fprintf(saida, "L%d:\n\t beq L%d\n", num, ++num);
+            else if (symbol == "THEN")
+                fprintf(saida, "L%d:\n", num++);
             else if (symbol == ";")
                 fprintf(saida, "mov pc, lr\n"); // function return
+            else if (symbol[0] == ":")
+            {
+                memmove(symbol, symbol+2, strlen(symbol));
+                fprintf("%s:\n", symbol);
+            }
             else
                 fprintf(saida, "bl %s\n", symbol); // branch to a label
         };
