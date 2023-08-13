06d7637cfaa52d9f6a32cd108cc1909a780ed944
Flute-Gamer <igorsouzalima01@gmail.com>
2023-08-13 17:19:35 -0300

    added logic operations

--- basic_functions.asm
+++ basic_functions.asm
@@ -17,69 +17,44 @@ dup:
     pop {r0}
     push {r0}
     push {r0}
-    mov pc, lr
+    mov pc, lr  
 
 neg:
     pop {r0}
     mov r1, #0
     sub, r0, r1, r0
     push {r0}
-    mov pc, lr
+    mov pc, lr  
 
 mul:
     pop {r0, r1}
     mul r0, r0, r1
     push {r0}
-    mov pc, lr
+    mov pc, lr  
 
 div:
     pop {r0, r1}
     div r0, r1, r0
     push {r0}
-    mov pc, lr
+    mov pc, lr  
 
 rot:
     pop {r0, r1, r2}
     push {r1}
     push {r0}
     push {r2}
-    mov pc, lr
+    mov pc, lr  
 
 swap:
     pop {r0, r1}
     push {r0}
     push {r1}
-    mov pc, lr
+    mov pc, lr  
 
 drop:
     add sp, #4
+    mov pc, lr  
     @ /* Movo stack pointer pra baixo */
-    mov pc, lr
-
-over:
-    pop {r0, r1}
-    push {r1}
-    push {r0}
-    push {r1}
-    mov pc, lr
-
-pick:
-    pop {r0}
-    mov r1, #4
-    mul r0, r0, r1
-    add sp, sp, r0
-    ldr r1, [sp]
-    sub sp, sp, r0
-    push {r1}
-    mov pc, lr
-
-tuck:
-    pop {r0}
-    pop {r1}
-    push {r0}
-    push {r1}
-    push {r0}
-    mov pc, lr
 
 @ /* Operacoes logicas */
 
@@ -91,10 +66,10 @@ tuck:
     pop {r2}
     b end
     greater:
-    mov r2, #1
-    pop {r2}
-    end
-    mov pc, lr
+        mov r2, #1
+        pop {r2}
+    end:
+        mov pc, lr  
 
 <:
     pop {r0, r1}
@@ -104,12 +79,12 @@ tuck:
     pop {r2}
     b end
     less:
-    mov r2, #1
-    pop {r2}
-    end
-    mov pc, lr
+        mov r2, #1
+        pop {r2}
+    end:
+        mov pc, lr  
 
-=:
+==:
     pop {r0, r1}
     cmp r1, r0
     beq equal
@@ -117,27 +92,91 @@ tuck:
     pop {r2}
     b end
     equal:
-    mov r2, #1
-    pop {r2}
-    end
-    mov pc, lr
+        mov r2, #1
+        pop {r2}
+    end:
+        mov pc, lr  
 
-<>:
+!=:
     pop {r0, r1}
     cmp r1, r0
     bne nequal
     mov r2, #0
     pop {r2}
     b end
-    nequal:
-    mov r2, #1
-    pop {r2}
-    end
-    mov pc, lr@ /* funcoes iniciais q tenho q fazer: Add Sub Mul Swap Dup Drop Rot */
+    equal:
+        mov r2, #1
+        pop {r2}
+    end:
+        mov pc, lr  
 
-add:
-    pop {ro, r1}
-    add r0, r0, r1
+@ /* Operacoes logicas com 0 */
+
+0=:
+    pop {r0}
+    comp r0, #0
+    beq equal
+    mov r2, #0
+    pop {r1}
+    b end
+    equal:
+        mov r2, #1
+        pop {r1}
+    end:
+        mov pc, lr  
+
+0<:
+    pop {r0}
+    cmp r0, #0
+    blt less
+    mov r2, #0
+    pop {r2}
+    b end
+        less:
+        mov r2, #1
+        pop {r2}
+    end:
+        mov pc, lr  
+
+0>:
+    pop {r0}
+    cmp r0, #0
+    bgt greater
+    mov r2, #0
+    pop {r2}
+    b end
+        greater:
+        mov r2, #1
+        pop {r2}
+    end:
+        mov pc, lr  
+
+@ /* Opoerações Lógicas or, and, negate */
+
+or:
+    pop {r0, r1}
+    orr r0, r0, r1
     push {r0}
     mov pc, lr  
+
+and: 
+    pop {ro, r1}
+    and r0, r0, r1
+    push {r0}
+    mov pc, lr  
+
+not:
+    pop {r0}
+    cmp {r0}
+    bgt diff
+    gle diff
+    @/* se nn deu branch, r0 e 0 msm ent, nn tem oq inverter */
+    b end
+    diff:     @/* r0 e diferente de 0, so fazer 0 - valor pra ter o valor negativo */
+        mov r1, #0
+        sub r0, r1, r0
+        b end
+    end:
+        push {r0}
+        mov pc, lr 
 
